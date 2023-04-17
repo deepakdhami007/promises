4dfd62f876f8334df39ad455674ef76b43130183
deepakdhami007 <deepaksinghdhami01@gmail.com>
2023-04-17 07:03:13 +0530

    first commit

---
+++ promise.js
@@ -0,0 +1,46 @@
+const posts = [];
+let lastActivityTime;
+
+function createPost(post) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      posts.push(post);
+      resolve();
+    }, 1000);
+  });
+}
+
+function updateLastUserActivityTime() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      lastActivityTime = new Date();
+      resolve();
+    }, 1000);
+  });
+}
+
+function deleteLastPost() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (posts.length > 0) {
+        const deletedPost = posts.pop();
+        resolve(deletedPost);
+      } else {
+        reject("ERROR: ARRAY IS EMPTY");
+      }
+    }, 1000);
+  });
+}
+
+createPost({ title: "POST1" })
+  .then(() => updateLastUserActivityTime())
+  .then(() => {
+    console.log("All posts:", posts);
+    console.log("Last activity time:", lastActivityTime);
+    return deleteLastPost();
+  })
+  .then((deletedPost) => {
+    console.log("Deleted post:", deletedPost);
+    console.log("Remaining posts:", posts);
+  })
+  .catch((error) => console.log(error));
